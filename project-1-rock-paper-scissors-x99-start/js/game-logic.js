5c7ddd2fac59540a1d4a36ff6d5ea1b2bacac28d
Liannedev <147238043+Liannedev@users.noreply.github.com>
2024-03-31 17:32:07 +0800

    Update game-logic.js

---
+++ project-1-rock-paper-scissors-x99-start/js/game-logic.js
@@ -0,0 +1,138 @@
+// All code should be written in this file.
+
+
+// NATHAN HERE
+// Jullie HERE
+// Nathan here
+var playerOneMoveOneType;
+var playerOneMoveTwoType;
+var playerOneMoveThreeType;
+
+
+var playerOneMoveOneValue;
+var playerOneMoveTwoValue;
+var playerOneMoveThreeValue;
+
+
+var playerTwoMoveOneType;
+var playerTwoMoveTwoType;
+var playerTwoMoveThreeType;
+
+
+var playerTwoMoveOneValue;
+var playerTwoMoveTwoValue;
+var playerTwoMoveThreeValue;
+
+const setPlayerMoves = (player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue) => {
+    if (player == "Player One") {
+        playerOneMoveOneType = moveOneType 
+        playerOneMoveOneValue = moveOneValue
+        playerOneMoveTwoType = moveTwoType
+        playerOneMoveTwoValue = moveTwoValue
+        playerOneMoveThreeType = moveThreeType
+        playerOneMoveThreeValue = moveThreeValue
+    } else if (player == "Player Two") {
+        playerTwoMoveOneType = moveOneType
+        playerTwoMoveOneValue = moveOneValue
+        playerOneMoveTwoType = moveTwoType
+        playerTwoMoveTwoValue = moveTwoValue
+        playerTwoMoveThreeType = moveThreeType
+        playerTwoMoveThreeValue = moveThreeValue
+    }
+}
+
+const getRoundWinner = (round) => {
+    if (playerOneMoveOneType === "rock" && playerTwoMoveOneType === "rock") {
+        return "Tie" 
+    } else if (playerOneMoveOneType === "rock" && playerTwoMoveOneType === "paper") {
+        return "Player Two" 
+    } else if (playerOneMoveOneType === "rock" && playerTwoMoveOneType === "scissors") {
+        return "Player One"
+    } else if (playerOneMoveOneType === "paper" && playerTwoMoveOneType === "rock") {
+        return "Player One" 
+    } else if (playerOneMoveOneType === "paper" && playerTwoMoveOneType === "paper") {
+        return "Tie" 
+    } else if (playerOneMoveOneType === "paper" && playerTwoMoveOneType === "scissors") {
+        return "Player Two"
+    } else if (playerOneMoveOneType === "scissors" && playerTwoMoveOneType === "rock") {
+        return "Player Two" 
+    } else if (playerOneMoveOneType === "scissors" && playerTwoMoveOneType === "paper") {
+        return "Player One" 
+    } else if (playerOneMoveOneType === "scissors" && playerTwoMoveOneType === "scissors") {
+        return "Tie"
+    } 
+
+}
+
+// jullie cute HERE
+// another 
+// All code should be written in this file.
+
+
+// NATHAN HERE
+// Jullie HERE
+
+var playerOneMoveOneType;
+var playerOneMoveTwoType;
+var playerOneMoveThreeType;
+
+
+var playerOneMoveOneValue;
+var playerOneMoveTwoValue;
+var playerOneMoveThreeValue;
+
+
+var playerTwoMoveOneType;
+var playerTwoMoveTwoType;
+var playerTwoMoveThreeType;
+
+
+var playerTwoMoveOneValue;
+var playerTwoMoveTwoValue;
+var playerTwoMoveThreeValue;
+
+const setPlayerMoves = (player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue) => {
+    if (player == "Player One") {
+        playerOneMoveOneType = moveOneType 
+        playerOneMoveOneValue = moveOneValue
+        playerOneMoveTwoType = moveTwoType
+        playerOneMoveTwoValue = moveTwoValue
+        playerOneMoveThreeType = moveThreeType
+        playerOneMoveThreeValue = moveThreeValue
+    } else if (player == "Player Two") {
+        playerTwoMoveOneType = moveOneType
+        playerTwoMoveOneValue = moveOneValue
+        playerOneMoveTwoType = moveTwoType
+        playerTwoMoveTwoValue = moveTwoValue
+        playerTwoMoveThreeType = moveThreeType
+        playerTwoMoveThreeValue = moveThreeValue
+    }
+}
+
+const getRoundWinner = (round) => {
+    if (playerOneMoveOneType === "rock" && playerTwoMoveOneType === "rock") {
+        return "Tie" 
+    } else if (playerOneMoveOneType === "rock" && playerTwoMoveOneType === "paper") {
+        return "Player Two" 
+    } else if (playerOneMoveOneType === "rock" && playerTwoMoveOneType === "scissors") {
+        return "Player One"
+    } else if (playerOneMoveOneType === "paper" && playerTwoMoveOneType === "rock") {
+        return "Player One" 
+    } else if (playerOneMoveOneType === "paper" && playerTwoMoveOneType === "paper") {
+        return "Tie" 
+    } else if (playerOneMoveOneType === "paper" && playerTwoMoveOneType === "scissors") {
+        return "Player Two"
+    } else if (playerOneMoveOneType === "scissors" && playerTwoMoveOneType === "rock") {
+        return "Player Two" 
+    } else if (playerOneMoveOneType === "scissors" && playerTwoMoveOneType === "paper") {
+        return "Player One" 
+    } else if (playerOneMoveOneType === "scissors" && playerTwoMoveOneType === "scissors") {
+        return "Tie"
+    } 
+
+}
+
+//NATHAN CUTIE HERE
+// jullie cute HERE
+// another 
+// jullie 3
